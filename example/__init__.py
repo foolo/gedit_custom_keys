a79bba560c276fd9737c59ac6f6e674175038c23
Olof Andersson <olof.andersson@gmail.com>
2019-11-12 20:48:15 +0100

    clean

--- example/__init__.py
+++ example/__init__.py
@@ -1,78 +1,15 @@
-# For more info visit the article https://theawless.github.io/How-to-write-plugins-for-gedit/
+from gi.repository import GObject, Gtk, Gedit, Gio
 
-# This file needs to be placed like ~/.local/share/gedit/plugins/example/__init__.py
-# or renamed like ~/.local/share/gedit/plugins/example.py depending on .plugin file
-
-from gi.repository import GObject, Gtk, Gedit, PeasGtk, Gio
-
-
-# For our example application, this class is not exactly required.
-# But we had to make it because we needed the app menu extension to show the menu.
 class ExampleAppActivatable(GObject.Object, Gedit.AppActivatable):
     app = GObject.property(type=Gedit.App)
-    __gtype_name__ = "ExampleAppActivatable"
+    window = GObject.property(type=Gedit.Window)
+    __gtype_name__ = "RedoCtrlYAppActivatable"
 
     def __init__(self):
         GObject.Object.__init__(self)
-        self.menu_ext = None
-        self.menu_item = None
 
     def do_activate(self):
         self.app.set_accels_for_action("win.redo", ("<Primary>Y", None))
 
     def do_deactivate(self):
         self.app.set_accels_for_action("win.redo", ())
-
-class ExampleWindowActivatable(GObject.Object, Gedit.WindowActivatable, PeasGtk.Configurable):
-    window = GObject.property(type=Gedit.Window)
-    __gtype_name__ = "ExampleWindowActivatable"
-
-    def __init__(self):
-        GObject.Object.__init__(self)
-        # This is the attachment we will make to bottom panel.
-        self.bottom_bar = Gtk.Box()
-    
-    #this is called every time the gui is updated
-    def do_update_state(self):
-        # if there is no document in sight, we disable the action, so we don't get NoneException
-        if self.window.get_active_view() is not None:
-            self.window.lookup_action('clear_document').set_enabled(true)
-
-    def do_activate(self):
-        # Defining the action which was set earlier in AppActivatable.
-        self._connect_menu()
-        self._insert_bottom_panel()
-
-    def _connect_menu(self):
-        action = Gio.SimpleAction(name='clear_document')
-        action.connect('activate', self.action_cb)
-        self.window.add_action(action)
-
-    def action_cb(self, action, data):
-        # On action clear the document.
-        doc = self.window.get_active_document()
-        doc.set_text("")
-
-    def _insert_bottom_panel(self):
-        # Add elements to panel.
-        self.bottom_bar.add(Gtk.Label("Hello There!"))
-        # Get bottom bar (A Gtk.Stack) and add our bar.        
-        panel = self.window.get_bottom_panel()
-        panel.add_titled(self.bottom_bar, 'example', "Example")
-        # Make sure everything shows up.
-        panel.show()
-        self.bottom_bar.show_all()
-        panel.set_visible_child(self.bottom_bar)
-
-    def do_deactivate(self):
-        self._remove_bottom_panel()
-
-    def _remove_bottom_panel(self):
-        panel = self.window.get_bottom_panel()
-        panel.remove(self.bottom_bar)
-
-    def do_create_configure_widget(self):
-        # Just return your box, PeasGtk will automatically pack it into a box and show it.
-        box=Gtk.Box()
-        box.add(Gtk.Label("Settings Here!"))
-        return box
