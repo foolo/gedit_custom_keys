96f6fad9593aaed949fdf9948c9af25c712022a1
Olof Andersson <olof.andersson@gmail.com>
2019-11-28 17:28:00 +0100

    comments

--- custom_keys.py
+++ custom_keys.py
@@ -5,46 +5,24 @@ class ExampleAppActivatable(GObject.Object, Gedit.AppActivatable):
 	__gtype_name__ = "CustomKeysAppActivatable"
 
 	def do_activate(self):
-		#  Uncomment/Comment any of the keyboard shortcuts below and edit as desired
+		#  Uncomment/comment any of the example keyboard shortcuts below and edit as desired
 		# "<Primary>" normally corresponds to Ctrl key
+		# See README.md for a list of known action names
 
 		self.add_keyboard_shortcut("win.redo", "<Primary>Y")
 		self.add_keyboard_shortcut("win.goto-line", "<Primary>G")
+		#Examples:
 		#self.add_keyboard_shortcut("app.new-window", "<Primary>N")
-		#self.add_keyboard_shortcut("app.quit", "<Primary>Q")
 		#self.add_keyboard_shortcut("app.help", "F1")
-		#self.add_keyboard_shortcut("win.hamburger-menu", "F10")
-		#self.add_keyboard_shortcut("win.open", "<Primary>O")
-		#self.add_keyboard_shortcut("win.save", "<Primary>S")
 		#self.add_keyboard_shortcut("win.save-as", "<Primary><Shift>S")
-		#self.add_keyboard_shortcut("win.save-all", "<Primary><Shift>L")
-		#self.add_keyboard_shortcut("win.new-tab", "<Primary>T")
 		#self.add_keyboard_shortcut("win.reopen-closed-tab", "<Primary><Shift>T")
-		#self.add_keyboard_shortcut("win.close", "<Primary>W")
-		#self.add_keyboard_shortcut("win.close-all", "<Primary><Shift>W")
-		#self.add_keyboard_shortcut("win.print", "<Primary>P")
-		#self.add_keyboard_shortcut("win.find", "<Primary>F")
-		#self.add_keyboard_shortcut("win.find-next", "<Primary>G")
-		#self.add_keyboard_shortcut("win.find-prev", "<Primary><Shift>G")
-		#self.add_keyboard_shortcut("win.replace", "<Primary>H")
-		#self.add_keyboard_shortcut("win.clear-highlight", "<Primary><Shift>K")
 		#self.add_keyboard_shortcut("win.focus-active-view", "Escape")
-		#self.add_keyboard_shortcut("win.side-panel", "F9")
 		#self.add_keyboard_shortcut("win.bottom-panel", "<Primary>F9")
-		#self.add_keyboard_shortcut("win.fullscreen", "F11")
 		#self.add_keyboard_shortcut("win.new-tab-group", "<Primary><Alt>N")
 		#self.add_keyboard_shortcut("win.previous-tab-group", "<Primary><Shift><Alt>Page_Up")
 		#self.add_keyboard_shortcut("win.next-tab-group", "<Primary><Shift><Alt>Page_Down")
 		#self.add_keyboard_shortcut("win.previous-document", "<Primary><Alt>Page_Up")
 		#self.add_keyboard_shortcut("win.next-document", "<Primary><Alt>Page_Down")
-
-
-		# Additional action names can be found by uncommenting the line below and run gedit from the command line:
-		# print(self.app.list_action_descriptions())
-
-		# More additional action names can be found in https://github.com/GNOME/gedit/blob/mainline/gedit/resources/gtk/menus-common.ui
-
-		# Note: Some shortcuts, such as Ctrl+D for delete line are hard-coded in function gedit_view_class_init in gedit-view.c
 
 	def do_deactivate(self):
 		self.app.set_accels_for_action("win.redo", ())
