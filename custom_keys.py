0f41825e890ed1d5f6466991e946f6d73bc3e353
Olof Andersson <olof.andersson@gmail.com>
2020-02-02 15:51:19 +0100

    changed spaces to tabs

--- custom_keys.py
+++ custom_keys.py
@@ -35,47 +35,47 @@ class ExampleAppActivatable(GObject.Object, Gedit.AppActivatable):
 
 # https://github.com/baxterross/GEdit3TabSwitch
 class GEdit3TabSwitch(GObject.Object, Gedit.WindowActivatable):
-    window = GObject.property(type=Gedit.Window)
+	window = GObject.property(type=Gedit.Window)
 
-    KEYS_LEFT = ('ISO_Left_Tab', 'Page_Up')
-    KEYS_RIGHT = ('Tab', 'Page_Down')
-    KEYS = KEYS_LEFT + KEYS_RIGHT
+	KEYS_LEFT = ('ISO_Left_Tab', 'Page_Up')
+	KEYS_RIGHT = ('Tab', 'Page_Down')
+	KEYS = KEYS_LEFT + KEYS_RIGHT
 
-    def __init__(self):
-        GObject.Object.__init__(self)
+	def __init__(self):
+		GObject.Object.__init__(self)
 
-    def do_activate(self):
-        handlers = []
-        handler_id = self.window.connect('key-press-event', self.on_key_press_event)
-        handlers.append(handler_id)
-        self.window.GEdit3TabSwitchHandlers = handlers
+	def do_activate(self):
+		handlers = []
+		handler_id = self.window.connect('key-press-event', self.on_key_press_event)
+		handlers.append(handler_id)
+		self.window.GEdit3TabSwitchHandlers = handlers
 
-    def do_deactivate(self):
-        handlers = self.window.GEdit3TabSwitchHandlers
-        for handler_id in handlers:
-            self.window.disconnect(handler_id)
+	def do_deactivate(self):
+		handlers = self.window.GEdit3TabSwitchHandlers
+		for handler_id in handlers:
+			self.window.disconnect(handler_id)
 
-    def do_update_state(self):
-        pass
+	def do_update_state(self):
+		pass
 
-    def on_key_press_event(self, window, event):
-        key = Gdk.keyval_name(event.keyval)
-        if event.state & Gdk.ModifierType.CONTROL_MASK and key in self.KEYS:
-            atab = window.get_active_tab()
-            tabs = atab.get_parent().get_children()
-            tlen = len(tabs)
-            i = 0
-            for tab in tabs:
-                i += 1
-                if tab == atab:
-                    break
-            if key in self.KEYS_LEFT:
-                i -= 2
-            if i < 0:
-                tab = tabs[tlen-1]
-            elif i >= tlen:
-                tab = tabs[0]
-            else:
-                tab = tabs[i]
-            window.set_active_tab(tab)
-            return True
+	def on_key_press_event(self, window, event):
+		key = Gdk.keyval_name(event.keyval)
+		if event.state & Gdk.ModifierType.CONTROL_MASK and key in self.KEYS:
+			atab = window.get_active_tab()
+			tabs = atab.get_parent().get_children()
+			tlen = len(tabs)
+			i = 0
+			for tab in tabs:
+				i += 1
+				if tab == atab:
+					break
+			if key in self.KEYS_LEFT:
+				i -= 2
+			if i < 0:
+				tab = tabs[tlen-1]
+			elif i >= tlen:
+				tab = tabs[0]
+			else:
+				tab = tabs[i]
+			window.set_active_tab(tab)
+			return True
